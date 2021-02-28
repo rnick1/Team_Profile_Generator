9e2dcb9ee3fd8b6fcd44d20184cea975d4c314c8
rnick1 <rnick0260@gmail.com>
2021-02-28 15:56:05 -0800

    Worked on employee and employee test.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,8 +1,16 @@
-const { it } = require('@jest/globals');
-const inquirer = require('inquirer');
-const employee = require('../lib/employee');
-const Letter = require('../lib/Letter');
+const Employee = require("../lib/employee");
 
-// describe('Employee class', () => {
-//     it(''
-// })
+describe("Employee", () => {
+    it("should create an employee object", () => {
+        const employee = new Employee('John', '3', 'john@fakemail.com');
+
+        expect(employee.name).toEqual(expect.any(String));
+        expect(employee.name).toEqual('employee')
+    })
+
+
+    test("should collect a string fromt the user", () => {
+        const name = 
+    )
+
+})
