11e3024b9e360309057a4808a0dd4e07e9b5c447
rnick1 <rnick0260@gmail.com>
2021-02-28 16:07:03 -0800

    Finished first draft of emloyee.js and Employee.test.js.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,31 @@
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
-    it("should create an employee object", () => {
+    test("should create an employee object", () => {
         const employee = new Employee('John', '3', 'john@fakemail.com');
 
         expect(employee.name).toEqual(expect.any(String));
-        expect(employee.name).toEqual('employee')
+        expect(employee.name).toEqual('John')
     })
 
+    test("getName method collects the employee's name from the user.", () => {
+        const employee = new Employee('John', '3', 'john@fakemail.com');
 
-    test("should collect a string fromt the user", () => {
-        const name = 
-    )
+        expect(employee.name).toEqual(expect.any(String))
+        expect(employee.name).toEqual('John')
+    })
 
+    test("getID method collects the employee's name from the user.", () => {
+        const employee = new Employee('John', '3', 'john@fakemail.com');
+
+        expect(employee.id).toEqual(expect.any(String))
+        expect(employee.id).toEqual('3')
+    })
+
+    test("getEmail method collects the employee's name from the user.", () => {
+        const employee = new Employee('John', '3', 'john@fakemail.com');
+
+        expect(employee.email).toEqual(expect.any(String))
+        expect(employee.email).toEqual('john@fakemail.com')
+    })
 })
