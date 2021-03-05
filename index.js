8e4b4289ef3ced0385e95b3c835d77208d313f20
rnick1 <rnick0260@gmail.com>
2021-03-04 16:03:42 -0800

    Fixed code so that it will generate cards.

--- index.js
+++ index.js
@@ -117,57 +117,94 @@ const employeeQuestions = () =>
                     console.log(internArray);
                     console.log(engineerArray);
 
-                    // TODO: Generate HTML from managerArray, internArray, and engineerArray, and
-                    // then have the HTML combined and saved to a file...
+                    // Important! => remember to use the .map method to allow multiple cards to be created.
+
+                    const getManagerCard = function (managerArray) {
+                        return `
+                        <div class="col mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">${managerArray[0].name}</h5>
+                                <ul class="card-text">
+                                    <li>Employee ID: ${managerArray[0].id}</li>
+                                    <li>Email: ${managerArray[0].email}</li>
+                                    <li>Office number: ${managerArray[0].officeNumber}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`
+                    }
+                    console.log(getManagerCard(managerArray));
+
+                    const engineerHTML = engineerArray.map(getEngineerCard(engineerArray))
+                    const internHTML = internArray.map(getInternCard())
+
+                    // .join("")
+                    const getEngineerCard = function (engineerArray) {
+                        return `
+                        <div class="col mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">${engineerArray.getName()}</h5>
+                                <ul class="card-text">
+                                    <li>Employee ID: ${engineerArray.getID()}</li>
+                                    <li>Employee email: ${engineerArray.getEmail()}</li>
+                                    <li>Github username: ${engineerArray.getGiHub()}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`
+                    }
+                    console.log(getEngineerCard(engineerArray));
+
+                    const getInternCard = function (internArray) {
+                        return `
+                        <div class="col mb-4">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">${internArray[0].name}</h5>
+                                <ul class="card-text">
+                                    <li>Employee ID:${internArray[0].id}</li>
+                                    <li> Employee email: ${internArray[0].email}</li>
+                                    <li>School attended: ${internArray[0].school}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>`
+                    }
+                    console.log(getInternCard(internArray));
+
+                    // const getManagerCard = arr.map(employee => employee.getManagerCard()).join("");
+
+                    return `
+                    <!DOCTYPE html>
+                    <html lang="en">
+
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+                            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+                        <title>Your Team Profile</title>
+                    </head>
+
+                    <body>
+                        <div class="jumbotron jumbotron-fluid">
+                            <div class="container">
+                                <h1 style="text-align: center">Here is Your Team:</h1>
+                                <div class="row row-cols-1 row-cols-md-3">    
+                                    ${getManagerCard}
+                                    ${engineerHTML}
+                                    ${internHTML}
+                                </div>
+                            </div>
+                        </div>
+                    </body>
+
+                </html>`
                 }
             })
         })
-
-function managerCard() {
-    // managerArray.forEach(Manager)
-    return `    
-    <div class="col mb-4">
-        <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">${manager.employeeName}</h5>
-                <ul class="card-text">
-                    <li>${manager.employeeID}</li>
-                    <li>${manager.employeeEmail}</li>
-                    <li>${manager.officeNumber}</li>
-                </ul>
-            </div>
-        </div>
-    </div>`
-}
-
-// const generateHTML = () => {
-
-//     `<!DOCTYPE html>
-//     <html lang="en">
-
-//     <head>
-//         <meta charset="UTF-8">
-//         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-//             integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-//         <title>Your Team Profile</title>
-//     </head>
-
-//     <body>
-//         <div class="jumbotron jumbotron-fluid">
-//             <div class="container">
-//                 <h1 style="text-align: center">Here is Your Team:</h1>
-//                 <div class="row row-cols-1 row-cols-md-3">    
-
-
-
-//                 </div>
-//             </div>
-//         </div>
-//     </body>
-
-// </html>`;
-// }
 
 const init = () => {
     employeeQuestions();
@@ -190,3 +227,17 @@ init();
         1. A card is generated containing all the employee information for each employee entered.
         2. An HTML file is generated containing each employee card.
 */
+
+
+// const engineerHTMLString = engineerArr.map(Engineer => 
+//     `<div class="card  " style="width: 18rem; background-color: blue; color: white; " >
+//     <div class="card-header">
+//       ${Engineer.getName()} <br>
+//       <i class="fas fa-glasses mr-2"></i>  Engineer
+//     </div>
+//     <ul class="list-group list-group-flush" style="color: black;" >
+//       <li class="list-group-item">ID:${Engineer.getId()}</li>
+//       <li class="list-group-item">Email: <a href="mailto: ">${Engineer.getEmail()}</a></li>
+//       <li class="list-group-item">GitHub:${Engineer.getGithub()}</li>
+//     </ul>
+// </div>`
